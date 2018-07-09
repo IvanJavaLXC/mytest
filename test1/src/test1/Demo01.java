d6ed676795b19ac582835054c7fe032c19f757cd
xlc <ivan_java_2016@163.com>
2018-07-09 17:50:40 +0800

    第一次提交代码

---
+++ test1/src/test1/Demo01.java
@@ -0,0 +1,22 @@
+/**   
+* @Title: Demo01.java 
+* @Package test1 
+* @Description: TODO(用一句话描述该文件做什么) 
+* @author 小许
+* @date 2018年7月9日 下午5:44:39 
+* @version V1.0   
+*/
+package test1;
+
+/** 
+* @ClassName: Demo01 
+* @Description: TODO(这里用一句话描述这个类的作用) 
+* @author 小许 
+* @date 2018年7月9日 下午5:44:39 
+*  
+*/
+public class Demo01 {
+public static void main(String[] args) {
+	System.out.println("我是git");
+}
+}
